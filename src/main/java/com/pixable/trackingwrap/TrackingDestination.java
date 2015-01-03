d9175d719720add7b95f3ba44ba81180ae6dfb60
Pablo Pera <pablo.sx@gmail.com>
2015-01-03 19:54:36 +0100

    Some docs

--- src/main/java/com/pixable/trackingwrap/TrackingDestination.java
+++ src/main/java/com/pixable/trackingwrap/TrackingDestination.java
@@ -1,5 +1,8 @@
 package com.pixable.trackingwrap;
 
+/**
+ * Destination for an event, that is, your analytics platform of choice.
+ */
 public enum TrackingDestination {
     MIXPANEL,
     GOOGLE_ANALYTICS;
--- src/main/java/com/pixable/trackingwrap/TrackingWrap.java
+++ src/main/java/com/pixable/trackingwrap/TrackingWrap.java
@@ -4,6 +4,10 @@ import android.util.Log;
 
 import java.util.Arrays;
 
+/**
+ * Entry point for the tracking wrap library. Build a wrap instance with your configuration and use
+ * it to send events to your analytics platforms of choice.
+ */
 public class TrackingWrap {
     private static final String TAG = TrackingWrap.class.getSimpleName();
 
@@ -13,6 +17,9 @@ public class TrackingWrap {
         this.configuration = configuration;
     }
 
+    /**
+     * Tracks the provided event in the provided destinations.
+     */
     public void trackEvent(TrackingEvent event, TrackingDestination... destinations) {
         if (configuration.getDebugPrints().contains(TrackingConfiguration.DebugPrint.LOGCAT)) {
             Log.d(TAG, "Track event " + event + " to " + Arrays.asList(destinations));

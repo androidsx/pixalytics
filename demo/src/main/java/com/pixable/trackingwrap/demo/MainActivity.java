1f9bce2e6f1a43cb813ecb5ccee10b31421e0958
Pablo Pera <pablo.sx@gmail.com>
2015-03-19 15:57:28 -0400

    Removed logic in the demo code
    In this type of code, a little duplication is totally fine, as it is
    there mostly for documentation purposes

--- demo/src/main/java/com/pixable/trackingwrap/demo/MainActivity.java
+++ demo/src/main/java/com/pixable/trackingwrap/demo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.pixable.trackingwrap.demo;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,9 +12,6 @@ import com.pixable.pixalytics.flurry.platform.FlurryPlatform;
 import com.pixable.pixalytics.ga.platform.GoogleAnalyticsPlatform;
 import com.pixable.pixalytics.mixpanel.platform.MixpanelPlatform;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class MainActivity extends TrackedActionBarActivity {
 
     @Override
@@ -25,37 +21,35 @@ public class MainActivity extends TrackedActionBarActivity {
         trackScreen();
     }
 
-    public void onFooFlurryClick(View view) {
-        trackEvent(FlurryPlatform.ID);
+    public void onFlurryClick(View view) {
+        Pixalytics.get().trackEvent(this, new Event.Builder().name("Foo").build(), FlurryPlatform.ID);
     }
 
-    public void onFooMixpanelClick(View view) {
-        trackEvent(MixpanelPlatform.ID);
-    }
-
-    public void onFooGoogleAnalyticsClick(View view) {
-        trackEvent(GoogleAnalyticsPlatform.ID);
-    }
-
-    public void onFooFacebookClick(View view) {
-        trackEvent(FacebookPlatform.ID);
-    }
-
-    private void trackEvent(String platformId) {
+    public void onMixpanelClick(View view) {
         Pixalytics.get().trackEvent(this,
-                new Event.Builder().name("foo")
-                        .property("view", "main")
-                        .property("property2", "value2")
-                        .property("property3", "value3")
+                new Event.Builder().name("Foo")
+                        .property("Key1", "Value1")
                         .build(),
-                platformId);
+                MixpanelPlatform.ID);
     }
 
-    private void trackScreen() {
-        Screen screen = new Screen.Builder().name("Main")
-                .property("1", "Value1")
-                .build();
-        Pixalytics.get().trackScreen(this, screen);
+    public void onGoogleAnalyticsClick(View view) {
+        Pixalytics.get().trackEvent(this,
+                new Event.Builder().name("Foo")
+                        .property("Key1", "Value1")
+                        .property("Key2", "Value2")
+                        .build(),
+                GoogleAnalyticsPlatform.ID);
+    }
+
+    public void onFacebookClick(View view) {
+        Pixalytics.get().trackEvent(this,
+                new Event.Builder().name("Foo")
+                        .property("Key1", "Value1")
+                        .property("Key2", "Value2")
+                        .property("Key3", "Value3")
+                        .build(),
+                FacebookPlatform.ID);
     }
 
     public void onAllPlatformsClick(View view) {
@@ -63,7 +57,17 @@ public class MainActivity extends TrackedActionBarActivity {
                 new Event.Builder().name("Bar")
                         .property("Key1", "Value1")
                         .property("Key2", "Value2")
+                        .property("Key3", "Value3")
+                        .property("Key4", "Value4")
+                        .property("Key5", "Value5")
                         .build(),
                 FlurryPlatform.ID, MixpanelPlatform.ID, GoogleAnalyticsPlatform.ID, FacebookPlatform.ID);
+    }
+
+    private void trackScreen() {
+        Screen screen = new Screen.Builder().name("Main")
+                .property("Key1", "Value1")
+                .build();
+        Pixalytics.get().trackScreen(this, screen);
     }
 }
